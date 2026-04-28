94efd69b837403611d1525dd8b77099716333184
Shekhar Manwar <shekhar.manwar@thinkitive.com>
2026-04-28 19:26:58 +0530

    Initial Commit

---
+++ AWSUploadTest/AWSUploadTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWSUploadTestApp.swift
+//  AWSUploadTest
+//
+//  Created by Mac on 28/04/26.
+//
+
+import SwiftUI
+
+@main
+struct AWSUploadTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AWSUploadTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AWSUploadTest
+//
+//  Created by Mac on 28/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
